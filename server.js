733fbe616e1deb1dc7500a65e51829c8622e628f
Glendoni <glendonsmall@yahoo.co.uk>
2018-10-24 16:59:05 +0100

    Update server.js
    Update to path

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/youteach'));
+app.use(express.static(__dirname + '/dist'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
