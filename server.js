6bb62c6020cce8341fdfc01f990bc4bc2cf6f1d1
Glendoni <glendonsmall@yahoo.co.uk>
2018-10-24 16:20:14 +0100

    Update server.js
    Changed path to pint to app name

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/dasha'));
+app.use(express.static(__dirname + '/dist/youteach'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/dasha/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/youteach/index.html'));
 });
 
 // default Heroku port
