3f1165de222bcf62f69421fa0f2ef898dc003aa7
Glendoni <glendonsmall@yahoo.co.uk>
2018-10-24 16:45:21 +0100

    Update server.js
    Update to path

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/dist/youteach'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/youteach/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // default Heroku port
